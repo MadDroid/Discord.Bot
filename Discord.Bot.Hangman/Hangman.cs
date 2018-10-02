8e82b3a598866a83dfe03433c5408d882b224c3f
Darlan Gabriel <darlan.skate.or.die@hotmail.com>
2018-10-02 20:23:50 -0300

    first commit

---
+++ Discord.Bot.Hangman/Hangman.cs
@@ -0,0 +1,149 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Discord.Bot.Hangman
+{
+    public class Hangman : ModuleBase<SocketCommandContext>
+    {
+        static public int TryesLeft { get; set; }
+        static public string UnderlinedWord { get; set; }
+        static public string CurrentWord { get; set; }
+        static public string CorrectGuesses { get; set; }
+        static public string IncorrectGuesses { get; set; }
+
+        static public string DefaultAlphabet
+        {
+            get
+            {
+                return "abcefghijklmnopqrstuvxwyz";
+            }
+        }
+
+        [Command("try")]
+        public async Task TryLetter(string argument)
+        {
+            if (argument.Length > 1)
+            {
+                await ReplyAsync("Você não pode chutar mais que uma letra por vez! ( " + argument.ToString() + " : " + (argument.Length - 1) + " letras excedidas )");
+
+                return;
+            }
+
+            var username = Program.LastUser.Username;
+
+            if (CurrentWord.ToLower().Contains(argument.ToLower()))
+            {
+                // await ReplyAsync("Acertou: " + "( " + username + " )" + GetUnderlinedWord(argument, CurrentWord));
+                CorrectGuesses += argument.ToLower();
+                await ReplyAsync("Acertou: " + "( " + username + " )" + GetUnderlinedWordRight());
+            }
+            else
+            {
+                if (TryesLeft > 1)
+                {
+                    await MissTryed(argument);
+
+                    await ReplyAsync("Errou: " + "( " + username + " )" + " tentativas restantes: " + TryesLeft);
+                }
+                else if (TryesLeft == 1)
+                {
+                    await MissTryed(argument);
+
+                    await ReplyAsync("Errou: " + "( " + username + " )" + " tentativas restantes: " + TryesLeft + " " + new Discord.Emoji(":que:"));
+
+                    await Task.Delay(1000);
+
+                    await ReplyAsync("Acabaram todas as tentativas desta palavra, a palavra era: " + CurrentWord);
+                }
+                else if (TryesLeft == 0)
+                {
+                    await ReplyAsync("Acabaram todas as tentativas desta palavra, a palavra era: " + CurrentWord);
+                }
+            }
+        }
+
+        private string GetUnderlinedWord(string tryed, string word)
+        {
+            string wordToCheck = word.ToLower();
+
+            string tryedValue = tryed.ToLower();
+
+            string valueReturn = string.Empty;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if ((word.ToLower())[i] == (tryed.ToLower())[0])
+                {
+                    valueReturn += " " + tryed[0].ToString() + " ";
+                }
+                else
+                {
+                    valueReturn += " - ";
+                }
+            }
+
+            return valueReturn;
+        }
+
+        private string GetUnderlinedWordRight()
+        {
+            string wordToCheck = CurrentWord.ToLower();
+
+            string valueReturn = string.Empty;
+
+            for (int i = 0; i < CurrentWord.Length; i++)
+            {
+                if (CompareChars(CurrentWord[i].ToString(), CorrectGuesses))
+                {
+                    valueReturn += " " + CurrentWord[i].ToString() + " ";
+                }
+                else
+                {
+                    valueReturn += " - ";
+                }
+            }
+
+            return valueReturn;
+        }
+
+        public bool CompareChars(string currentIndex, string build)
+        {
+            if (build.ToLower().Contains(currentIndex.ToLower()))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        [Command("restart")]
+        public async Task RestartGame()
+        {
+            TryesLeft = 6;
+
+            CorrectGuesses = string.Empty;
+
+            await ReplyAsync("Jogo reiniciado e tentativas zeradas!");
+        }
+
+        [Command("tryes")]
+        public async Task DebuTryesLeft()
+        {
+            await ReplyAsync("Tentativas restantes: " + TryesLeft);
+        }
+
+
+        public Task MissTryed(string arg)
+        {
+            Console.WriteLine(arg);
+
+            TryesLeft--;
+
+            IncorrectGuesses += arg.ToLower();
+
+            return Task.CompletedTask;
+        }
+    }
+}
--- Discord.Bot.Hangman/Program.cs
+++ Discord.Bot.Hangman/Program.cs
@@ -1,12 +1,103 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord.Net;
+using Discord.WebSocket;
+using Discord.Commands;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using Discord;
 
 namespace Discord.Bot.Hangman
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+        static public SocketUser LastUser { get; set; }
+
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+
+        private async Task RunBotAsync()
         {
-            Console.WriteLine("Hello World!");
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+            // Depency injection based on the client and commands to get the infos
+            _services = new ServiceCollection().AddSingleton(_client).AddSingleton(_commands).BuildServiceProvider();
+
+            string botToken = "NDk2MTkyOTIwMzQxMzE1NjA2.DpQtiQ.ourSV43SSxSiJbxwMfXvw6JHbIQ";
+
+            LastUser = null;
+
+            Hangman.CurrentWord = "Palavra";
+
+            // events inputs
+
+            Hangman.TryesLeft = 6;
+
+            _client.Log += Log;
+
+            await RegisterCommandAsync();
+
+            await _client.LoginAsync(TokenType.Bot, botToken);
+
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        public Task Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly());
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+
+            // if the command is null or who typed is a bot
+
+            if (message == null || message.Author.IsBot)
+            {
+                return;
+            }
+
+            int argumentIndexPosition = 0;
+
+            if (message.HasStringPrefix("!", ref argumentIndexPosition) || message.HasMentionPrefix(_client.CurrentUser, ref argumentIndexPosition))
+            {
+                await UpdateLastUser(arg);
+
+                var context = new SocketCommandContext(_client, message);
+
+                var result = await _commands.ExecuteAsync(context, argumentIndexPosition, _services);
+
+                if (!result.IsSuccess)
+                {
+                    Console.WriteLine(result.ErrorReason);
+                }
+            }
+        }
+
+        static public Task UpdateLastUser(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+
+            LastUser = message.Author;
+
+            return Task.CompletedTask;
         }
     }
 }
